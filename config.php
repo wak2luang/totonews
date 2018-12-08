19d757d40b4800b92953c739c150fcedaabeb2ab
wak2luang <45333585+wak2luang@users.noreply.github.com>
2018-12-08 19:35:37 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,6 @@
 //ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
 //jangan ada sepasi
 $config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBin-Zr9CjbbSle8u8jXqyina7zo7chWS0iIVlkKO33IqKhM-wuoVptLiGlq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=942471dfcb423d90",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh6LRr9Cfr7-Tdtq8oH5riZ-22o_PhWW1hXFmhI23louKh5e64J-vtaiHza2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=6a0e2ee12d25527",
   ];
-
 ?>
